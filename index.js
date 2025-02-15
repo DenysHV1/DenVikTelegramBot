93fe92adbe69e568085abc8edefef95e9bf7e479
DenVik <denyagarkusha2@gmail.com>
2025-02-15 21:55:16 +0100

    add PORT for build on Cloud

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ import { gameResponser } from './responsers/gameResponser.js';
 bot.setMyCommands(commands);
 bot.on('message', denVikResponser);
 bot.on('callback_query', gameResponser);
+
+
+//Fake
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
